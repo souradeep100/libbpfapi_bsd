a98bbccc5079d8b1a327b91885c699d75e23c440
edguer <edguer@microsoft.com>
2022-09-21 21:10:01 -0300

    introducing

--- src/service/bpf_svc.c
+++ src/service/bpf_svc.c
@@ -6,6 +6,11 @@
 
 #include "../prototypes/bpf_svc.h"
 
+static void log_info(const char* fmt, ...)
+{
+    // TODO: logging
+}
+
 static char* get_verifier_path()
 {
     char* path = getenv("PREVAIL_PATH");
@@ -24,12 +29,12 @@ int * ebpf_verify_load_program_1_svc(ebpf_verify_and_load_arg *args, struct svc_
     static int result = 0;
     if (!args)
     {
-        // printf("Arguments are empty\n");
+        log_info("Arguments are empty\n");
         result = -1;
         return &result;
     }
 
-    // printf("Verifying BPF program...");
+    log_info("Verifying BPF program...");
 
     return &result;
 }
@@ -40,42 +45,42 @@ edpf_verify_result * ebpf_verify_program_1_svc(ebpf_verify_arg *args, struct svc
     result.result = EBPF_VERIFIER_NOT_PROCESSED;
     result.message = NULL;
     char* byte_code_path = args->path;
-    // printf("Received request, running check program, bytecode path is %s", byte_code_path);
+    log_info("Received request, running check program, bytecode path is %s", byte_code_path);
 
     int fds[2];
     if (pipe(fds) == -1) {
-        // printf("Unable to stand up pipe from/to verifier.\n");
+        log_info("Unable to stand up pipe from/to verifier.\n");
         result.result = EBPF_VERIFIER_CALL_ERR;
         result.message = "Unable to stand up pipe from/to verifier";
         return &result;
     }
 
-    // printf("Created pipe...\n");
+    log_info("Created pipe...\n");
 
     pid_t pid = fork();
-    // printf("Forked...\n");
+    log_info("Forked...\n");
     if (pid == 0)
     {
-        // printf("Starting child process...\n");
+        log_info("Starting child process...\n");
         while ((dup2(fds[1], STDOUT_FILENO) == -1)) {}
         while ((dup2(fds[1], STDERR_FILENO) == -1)) {}
 
-        // printf("Closing file descriptors...\n");
+        log_info("Closing file descriptors...\n");
         close(fds[1]);
         close(fds[0]);
 
-        // printf("Calling verifier...\n");
+        log_info("Calling verifier...\n");
         char *args[] = { "check", byte_code_path, "2/1",  "--domain=zoneCrab", NULL };
         int status = execvp(get_verifier_path(), args);
 
-        // printf("Failed to execute command: %i\n", status);
+        log_info("Failed to execute command: %i\n", status);
         exit(status);
     }
 
-    // printf("Closing parent file descriptor...\n");
+    log_info("Closing parent file descriptor...\n");
     close(fds[1]);
 
-    // printf("Waiting child process...\n");
+    log_info("Waiting child process...\n");
     int check_status;
     waitpid(pid, &check_status, 0);
 
@@ -83,17 +88,17 @@ edpf_verify_result * ebpf_verify_program_1_svc(ebpf_verify_arg *args, struct svc
 
     if (WIFSIGNALED(check_status))
     {
-        // printf("Verifier process ended abnormally, exit code: %i\n", verifier_exit_code);
+        log_info("Verifier process ended abnormally, exit code: %i\n", verifier_exit_code);
         result.result = EBPF_VERIFIER_ABNOR_EXIT;
         result.message = "Verifier process ended abnormally";
         return &result;
     }
 
-    // printf("Verifier process exited, exit code: %i\n", verifier_exit_code);
+    log_info("Verifier process exited, exit code: %i\n", verifier_exit_code);
 
     if (verifier_exit_code != 0)
     {
-        // printf("Verifier process exited with non-zero value, exiting... Exit code: %i\n", verifier_exit_code);
+        log_info("Verifier process exited with non-zero value, exiting... Exit code: %i\n", verifier_exit_code);
         result.result = EBPF_VERIFIER_NON_ZERO_EXIT;
     }
 
@@ -106,16 +111,16 @@ edpf_verify_result * ebpf_verify_program_1_svc(ebpf_verify_arg *args, struct svc
     ssize_t count = read(fds[0], buffer, sizeof(buffer));
     if (count == -1)
     {
-        // printf("Error reading verifier output, count is %i\n", (int)count);
+        log_info("Error reading verifier output, count is %i\n", (int)count);
         result.result = EBPF_VERIFIER_CALL_ERR;
         return &result;
     }
     else if (count == 0)
     {
-        // printf("Verifier's STDOUT is empty...\n");
+        log_info("Verifier's STDOUT is empty...\n");
     }
 
-    // printf("Verifier output: %s\n", &buffer[0]);
+    log_info("Verifier output: %s\n", &buffer[0]);
     result.message = &buffer[0];
     if (result.result == EBPF_VERIFIER_NON_ZERO_EXIT)
     {
@@ -127,12 +132,12 @@ edpf_verify_result * ebpf_verify_program_1_svc(ebpf_verify_arg *args, struct svc
     char verifier_status = buffer[0];
     if (verifier_status == '1')
     {
-        // printf("Succeeded, verifier status: %s\n", &verifier_status);
+        log_info("Succeeded, verifier status: %s\n", &verifier_status);
         result.result = EBPF_VERIFIER_PASS;
     }
     else
     {
-        // printf("Verifier returned non-success code: %s\n", &verifier_status);
+        log_info("Verifier returned non-success code: %s\n", &verifier_status);
         result.result = EBPF_VERIFIER_INVALID;
     }
 
