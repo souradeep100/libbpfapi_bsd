29fd8fa691218086d6cc26f4cf3cbb2cee84cfe1
edguer <edguer@microsoft.com>
2022-09-26 21:31:25 -0300

    introducing

--- external/ebpf-verifier/ebpf-samples/asm/tail_call_bad.asm
+++
@@ -1,4 +0,0 @@
-; /home/alanjo/ebpf-verifier/ebpf-samples/src/tail_call_bad.c:37
-;     return 42;
-       0:	r0 = 42
-       1:	exit
--- external/ebpf-verifier/src/linux/linux_platform.hpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Prevail Verifier contributors.
-// SPDX-License-Identifier: MIT
-#pragma once
-
-EbpfHelperPrototype get_helper_prototype_linux(int32_t n);
-bool is_helper_usable_linux(int32_t n);
